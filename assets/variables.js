e0a21655f849f69ae3e265575687d3c358fb8d92
Jorge Ariel Castillo <j.a.castillo.jc@gmail.com>
2022-10-20 14:48:27 -0300

    se creo array de productos, se creo html de categorias

---
+++ assets/variables.js
@@ -0,0 +1,173 @@
+let products = [
+    {
+        id: 1,
+        name: "Kangoroo Hoddie HTML",
+        category: "Hoddie",
+        price: 250,
+        technologie: "html",
+        collection: "frontend",
+        size: ["small", "large", "medium"],
+        img: "/assets/img/front/hoddie-html.png"
+    }, {
+        id: 2,
+        name: "T-Shirt CSS3",
+        category: "T-Shirt",
+        price: 100,
+        technologie: "css3",
+        collection: "frontend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/front/tshirt-css.png"
+    }, {
+        id: 3,
+        name: "Sweatshirt SASS",
+        category: "sweatshirt",
+        price: 200,
+        technologie: "sass",
+        collection: "frontend",
+        size: ["small", "large", "medium", "extra-large", "extra-extra-large"],
+        img: "/assets/img/front/sweat-sass.png"
+    }, {
+        id: 4,
+        name: "Kangoroo Hoddie UNITY",
+        category: "Hoddie",
+        price: 250,
+        technologie: "unity",
+        collection: "frontend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/front/hoddie-unity.png"
+    }, {
+        id: 5,
+        name: "T-Shirt GATSBY",
+        category: "T-Shirt",
+        price: 100,
+        technologie: "gatsby",
+        collection: "frontend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/front/tshirt-gatsby.png"
+    },{
+        id: 6,
+        name: "Kangoroo Hoddie REACT",
+        category: "Hoddie",
+        price: 250,
+        technologie: "react",
+        collection: "frontend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/front/hoddie-react.png"
+    },{
+        id: 7,
+        name: "Sweatshirt REACT",
+        category: "sweatshirt",
+        price: 200,
+        technologie: "react",
+        collection: "frontend",
+        size: ["small", "large", "medium", "extra-large", "extra-extra-large"],
+        img: "/assets/img/front/sweat-react.png"
+    },{
+        id: 8,
+        name: "Kangoroo Hoddie JAVASCRIPT",
+        category: "Hoddie",
+        price: 250,
+        technologie: "javascript",
+        collection: "frontend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/front/hoddie-js.png"
+    },{
+        id: 9,
+        name: "T-Shirt VUE-JS",
+        category: "T-Shirt",
+        price: 100,
+        technologie: "vue-js",
+        collection: "frontend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/front/tshirt-vue.png"
+    },{
+        id: 10,
+        name: "Sweatshirt TYPESCRIPT",
+        category: "sweatshirt",
+        price: 200,
+        technologie: "typescript",
+        collection: "frontend",
+        size: ["small", "large", "medium", "extra-large", "extra-extra-large"],
+        img: "/assets/img/front/sweat-ts.png"
+    },{
+        id: 11,
+        name: "Sweatshirt RUBY",
+        category: "sweatshirt",
+        price: 200,
+        technologie: "ruby",
+        collection: "backend",
+        size: ["small", "large", "medium", "extra-large", "extra-extra-large"],
+        img: "/assets/img/back/sweat-ruby.png"
+    },{
+        id: 12,
+        name: "Kangoroo Hoddie JAVASCRIPT",
+        category: "Hoddie",
+        price: 250,
+        technologie: "javascript",
+        collection: "backend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/back/hoddie-js-logo.png"
+    },{
+        id: 13,
+        name: "Kangoroo Hoddie KOTLIN",
+        category: "Hoddie",
+        price: 250,
+        technologie: "kotlin",
+        collection: "backend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/back/hoddie-kotlin.png"
+    },{
+        id: 14,
+        name: "Kangoroo Hoddie PHP",
+        category: "Hoddie",
+        price: 250,
+        technologie: "php",
+        collection: "backend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/back/hoddie-php.png"
+    },{
+        id: 14,
+        name: "Kangoroo Hoddie PYTHON",
+        category: "Hoddie",
+        price: 250,
+        technologie: "python",
+        collection: "backend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/back/hoddie-python.png"
+    },{
+        id: 15,
+        name: "T-Shirt C#",
+        category: "T-Shirt",
+        price: 100,
+        technologie: "c#",
+        collection: "backend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/back/tshirt-c.png"
+    },{
+        id: 16,
+        name: "T-Shirt C++",
+        category: "T-Shirt",
+        price: 100,
+        technologie: "c++",
+        collection: "backend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/back/tshirt-c++.png"
+    },{
+        id: 17,
+        name: "T-Shirt MYSQL",
+        category: "T-Shirt",
+        price: 100,
+        technologie: "mysql",
+        collection: "backend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/back/tshirt-mysql.png"
+    },{
+        id: 14,
+        name: "Kangoroo Hoddie MONGODB",
+        category: "Hoddie",
+        price: 250,
+        technologie: "mongodb",
+        collection: "backend",
+        size: ["small", "large", "medium", "extra-large"],
+        img: "/assets/img/back/hoddie-mongo.png"
+    }]
