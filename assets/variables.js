15e789b2488902dc2ad3ecf9d336b1271a2f9047
Jorge Ariel Castillo <j.a.castillo.jc@gmail.com>
2022-10-31 00:27:46 -0300

    se cargaron mas imagenes y se agregaron a los productos

--- assets/variables.js
+++ assets/variables.js
@@ -21,7 +21,12 @@ let products = [
         technologie: "css3",
         collection: "frontend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/front/tshirt-css.png"
+        img: "/assets/img/front/tshirt-css.png",
+        imgBig: "/assets/img/front/tshirt-css-big.png",
+        imgGallery1: "/assets/img/front/tshirt-css-gallery1.png",
+        imgGallery2: "/assets/img/front/tshirt-css-gallery2.png",
+        imgGallery3: "/assets/img/front/tshirt-css-gallery3.png",
+        imgGallery4: "/assets/img/front/tshirt-css-gallery4.png",
     }, {
         id: 3,
         name: "Sweatshirt SASS",
@@ -39,7 +44,12 @@ let products = [
         technologie: "unity",
         collection: "frontend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/front/hoddie-unity.png"
+        img: "/assets/img/front/hoddie-unity.png",
+        imgBig: "/assets/img/front/hoodie-unity-big.png",
+        imgGallery1: "/assets/img/front/hoodie-unity-gallery1.png",
+        imgGallery2: "/assets/img/front/hoodie-unity-gallery2.png",
+        imgGallery3: "/assets/img/front/hoodie-unity-gallery3.png",
+        imgGallery4: "/assets/img/front/hoodie-unity-gallery4.png",
     }, {
         id: 5,
         name: "T-Shirt GATSBY",
@@ -48,7 +58,12 @@ let products = [
         technologie: "gatsby",
         collection: "frontend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/front/tshirt-gatsby.png"
+        img: "/assets/img/front/tshirt-gatsby.png",
+        imgBig: "/assets/img/front/tshirt-gatsby-big.png",
+        imgGallery1: "/assets/img/front/tshirt-gatsby-gallery1.png",
+        imgGallery2: "/assets/img/front/tshirt-gatsby-gallery2.png",
+        imgGallery3: "/assets/img/front/tshirt-gatsby-gallery3.png",
+        imgGallery4: "/assets/img/front/tshirt-gatsby-gallery4.png",
     },{
         id: 6,
         name: "Kangaroo Hoddie REACT",
@@ -80,7 +95,12 @@ let products = [
         technologie: "javascript",
         collection: "frontend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/front/hoddie-js.png"
+        img: "/assets/img/front/hoddie-js.png",
+        imgBig: "/assets/img/front/hoodie-javascript-big.png",
+        imgGallery1: "/assets/img/front/hoodie-javascript-gallery1.png",
+        imgGallery2: "/assets/img/front/hoodie-javascript-gallery2.png",
+        imgGallery3: "/assets/img/front/hoodie-javascript-gallery3.png",
+        imgGallery4: "/assets/img/front/hoodie-javascript-gallery4.png",
     },{
         id: 9,
         name: "T-Shirt VUE-JS",
@@ -89,7 +109,12 @@ let products = [
         technologie: "vue-js",
         collection: "frontend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/front/tshirt-vue.png"
+        img: "/assets/img/front/tshirt-vue.png",
+        imgBig: "/assets/img/front/tshirt-vue-big.png",
+        imgGallery1: "/assets/img/front/tshirt-vue-gallery1.png",
+        imgGallery2: "/assets/img/front/tshirt-vue-gallery2.png",
+        imgGallery3: "/assets/img/front/tshirt-vue-gallery3.png",
+        imgGallery4: "/assets/img/front/tshirt-vue-gallery4.png",
     },{
         id: 10,
         name: "Sweatshirt TYPESCRIPT",
@@ -116,7 +141,12 @@ let products = [
         technologie: "javascript",
         collection: "backend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/back/hoddie-js-logo.png"
+        img: "/assets/img/back/hoddie-js-logo.png",
+        imgBig: "/assets/img/back/hoddie-js-logo-big.png",
+        imgGallery1: "/assets/img/back/hoodie-javascript-b-gallery1.png",
+        imgGallery2: "/assets/img/back/hoodie-javascript-b-gallery2.png",
+        imgGallery3: "/assets/img/back/hoodie-javascript-b-gallery3.png",
+        imgGallery4: "/assets/img/back/hoodie-javascript-b-gallery4.png",
     },{
         id: 13,
         name: "Kangaroo Hoddie KOTLIN",
@@ -125,7 +155,12 @@ let products = [
         technologie: "kotlin",
         collection: "backend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/back/hoddie-kotlin.png"
+        img: "/assets/img/back/hoddie-kotlin.png",
+        imgBig: "/assets/img/back/hoddie-kotlin-big.png",
+        imgGallery1: "/assets/img/back/hoodie-kotlin-gallery1.png",
+        imgGallery2: "/assets/img/back/hoodie-kotlin-gallery2.png",
+        imgGallery3: "/assets/img/back/hoodie-kotlin-gallery3.png",
+        imgGallery4: "/assets/img/back/hoodie-kotlin-gallery4.png",
     },{
         id: 14,
         name: "Kangaroo Hoddie PHP",
@@ -134,27 +169,42 @@ let products = [
         technologie: "php",
         collection: "backend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/back/hoddie-php.png"
+        img: "/assets/img/back/hoddie-php.png",
+        imgBig: "/assets/img/back/hoodie-php-big.png",
+        imgGallery1: "/assets/img/back/hoodie-php-gallery1.png",
+        imgGallery2: "/assets/img/back/hoodie-php-gallery2.png",
+        imgGallery3: "/assets/img/back/hoodie-php-gallery3.png",
+        imgGallery4: "/assets/img/back/hoodie-php-gallery4.png",
     },{
-        id: 14,
+        id: 15,
         name: "Kangaroo Hoddie PYTHON",
         category: "Hoddie",
         price: 250,
         technologie: "python",
         collection: "backend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/back/hoddie-python.png"
+        img: "/assets/img/back/hoddie-python.png",
+        imgBig: "/assets/img/back/hoddie-python-big.png",
+        imgGallery1: "/assets/img/back/hoodie-python-gallery1.png",
+        imgGallery2: "/assets/img/back/hoodie-python-gallery2.png",
+        imgGallery3: "/assets/img/back/hoodie-python-gallery3.png",
+        imgGallery4: "/assets/img/back/hoodie-python-gallery4.png",
     },{
-        id: 15,
+        id: 16,
         name: "T-Shirt C#",
         category: "T-Shirt",
         price: 100,
         technologie: "c#",
         collection: "backend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/back/tshirt-c.png"
+        img: "/assets/img/back/tshirt-c.png",
+        imgBig: "/assets/img/back/tshirt-c-big.png",
+        imgGallery1: "/assets/img/back/tshirt-c-gallery1.png",
+        imgGallery2: "/assets/img/back/tshirt-c-gallery2.png",
+        imgGallery3: "/assets/img/back/tshirt-c-gallery3.png",
+        imgGallery4: "/assets/img/back/tshirt-c-gallery4.png",
     },{
-        id: 16,
+        id: 17,
         name: "T-Shirt C++",
         category: "T-Shirt",
         price: 100,
@@ -163,7 +213,7 @@ let products = [
         size: ["small", "large", "medium", "extra-large"],
         img: "/assets/img/back/tshirt-c++.png"
     },{
-        id: 17,
+        id: 18,
         name: "T-Shirt MYSQL",
         category: "T-Shirt",
         price: 100,
@@ -172,16 +222,21 @@ let products = [
         size: ["small", "large", "medium", "extra-large"],
         img: "/assets/img/back/tshirt-mysql.png"
     },{
-        id: 18,
+        id: 19,
         name: "Kangaroo Hoddie MONGODB",
         category: "Hoddie",
         price: 250,
         technologie: "mongodb",
         collection: "Backend",
         size: ["small", "large", "medium", "extra-large"],
-        img: "/assets/img/back/hoddie-mongo.png"
+        img: "/assets/img/back/hoddie-mongo.png",
+        imgBig: "/assets/img/back/hoodie-mongo-big.png",
+        imgGallery1: "/assets/img/back/hoodie-mongo-gallery1.png",
+        imgGallery2: "/assets/img/back/hoodie-mongo-gallery2.png",
+        imgGallery3: "/assets/img/back/hoodie-mongo-gallery3.png",
+        imgGallery4: "/assets/img/back/hoodie-mongo-gallery4.png",
     },{
-        id: 19,
+        id: 20,
         name: "Snapback REACT",
         category: "Snapback",
         price: 50,
@@ -195,7 +250,7 @@ let products = [
         imgGallery3: "/assets/img/front/snapback-react-gallery3.png",
         imgGallery4: "/assets/img/front/snapback-react-gallery4.png",
     },{
-        id: 20,
+        id: 21,
         name: "Snapback Sass",
         category: "Snapback",
         price: 50,
